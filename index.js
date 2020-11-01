373fb4ee00f531f468d3cf9291eab83738a8d22b
Lee Isles <leeisles@gmail.com>
2020-11-01 15:42:09 -0500

    write stdout

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var chance = new Chance();
 
 
 function logging(msg) {
-  console.log(`[${DateTime.utc().toISO()}]::${msg}`);
+  process.stdout.write(`[${DateTime.utc().toISO()}]::${msg}`);
 }
 
 (async () => {
