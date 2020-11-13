fd1bc7ab5a421fb4205c7df5940d7225f9d87fef
Lee Isles <leeisles@gmail.com>
2020-11-13 12:26:12 -0500

    add random wait

--- index.js
+++ index.js
@@ -32,70 +32,77 @@ function logging(msg) {
     // in the start/end window minus 1
     const shouldPost = chance.weighted([false, true], [nHoursOpen/2, 1])
     if (shouldPost) {
+      // prep a timeout so we can vary the actual post time
+      let minMin = 120000 // 5 mins
+      let maxMin = 900000 // 15
+      const ms = Math.floor( Math.random() * (maxMin - minMin) + minMin );
+      setTimeout( () => {
 
-      // get random file from s3
-      const randomFile = await Files.randomFile()
-      logging(`file: ${randomFile}`);
-      
-      // get team info
-      const team = await web.team.info();
-      const teamID = team.id;
+        // get random file from s3
+        const randomFile = await Files.randomFile()
+        logging(`file: ${randomFile}`);
+        
+        // get team info
+        const team = await web.team.info();
+        const teamID = team.id;
 
-      // get users, randomNancy
-      const users = await web.users.list({team_id: teamID});
-      const randomNancyUser = users.members.filter( (user) => { 
-        return user.name == 'randomnancy' 
-      })
+        // get users, randomNancy
+        const users = await web.users.list({team_id: teamID});
+        const randomNancyUser = users.members.filter( (user) => { 
+          return user.name == 'randomNancy' 
+        })
 
-      // invite RandomNancy to all channels, see what's in em 
-      const conversations = await web.conversations.list();  
-      const channels = conversations.channels;
-      
-      let channelAggs = [];
-      // loop through the channels
-      for (channel of channels) {      
+        // invite RandomNancy to all channels, see what's in em 
+        const conversations = await web.conversations.list();  
+        const channels = conversations.channels;
+        
+        // loop through the channels
+        let channelAggs = [];
+        for (channel of channels) {      
 
-        // randomNancy joins -- she must be in the channel to do history
-        try {
-          await web.conversations.join({channel: channel.id});
+          // randomNancy joins -- she must be in the channel to do history
+          try {
+            await web.conversations.join({channel: channel.id});
 
-          // get all the messages for analysis
-          const channelHistory = await web.conversations.history({channel: channel.id})
-          let maxTS = Math.max.apply(Math, channelHistory.messages.map(function(o) { return o.ts; }))
+            // get all the messages for analysis
+            const channelHistory = await web.conversations.history({channel: channel.id})
+            let maxTS = Math.max.apply(Math, channelHistory.messages.map(function(o) { return o.ts; }))
 
-          channelAggs.push({
-            channelID: channel.id,
-            maxTS: maxTS,
-            numberMessages: channelHistory.messages.length});    
+            channelAggs.push({
+              channelID: channel.id,
+              maxTS: maxTS,
+              numberMessages: channelHistory.messages.length});    
 
-        } catch (joinError) {
-          logging(joinError)
+          } catch (joinError) {
+            logging(joinError)
+          }
         }
-      }
 
-      const max = channelAggs.reduce( function(prev, current) {
-        return (prev.maxTS > current.maxTS) ? prev : current
-      }) //returns object
+        const max = channelAggs.reduce( function(prev, current) {
+          return (prev.maxTS > current.maxTS) ? prev : current
+        }) //returns object (channel with most recent activity)
 
-      // post a message
-      if (!debug) {
-        try {
-          const postResponse = await web.chat.postMessage({
-            "channel": max.channelID,
-            "blocks": [
-              {
-                "type": "image",
-                "image_url": randomFile,
-                "alt_text": "randomNancy Image"
-              }
-            ]
-          })
-          logging(JSON.stringify(postResponse));
-        } catch (error) {
-          logging(error);
+        // post a message
+        if (!debug) {
+          try {
+            const postResponse = await web.chat.postMessage({
+              "channel": max.channelID,
+              "blocks": [
+                {
+                  "type": "image",
+                  "image_url": randomFile,
+                  "alt_text": "randomNancy Image"
+                }
+              ]
+            })
+            logging(JSON.stringify(postResponse));
+          } catch (error) {
+            logging(error);
+          }
         }
-      }
-      else {logging('would have posted, but debugging')}
+        else {logging('would have posted, but debugging')}
+
+      }, ms); // end setTimeOut
 
     } else logging('no chance')
 
