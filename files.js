81bf447dd15aceae97257b680b04216a786a7e91
Lee Isles <leeisles@gmail.com>
2020-11-23 11:58:02 -0500

    remove duplicates from possible set of files

--- files.js
+++ files.js
@@ -1,5 +1,6 @@
 
 const AWS = require('aws-sdk');
+const { DateTime, Interval } = require('luxon');
 const settings = require('./settings.json');
 const { getLogRows } = require('./db');
 
@@ -24,22 +25,31 @@ exports.allFiles = async () => {
 
 exports.randomFile = async () => {
   // send a randomly selected file
-  let awsFiles = await this.allFiles();
-  let logFiles = await this.getLogFiles();
+  const logs = await getLogRows();
+  const awsFileObjects = await this.allFiles();  
 
-  let pickFile = function(awsFiles, logFiles) {
-    function randomFile(awsFiles) {
-      return settings.config.aws.S3_URL + awsFiles[ Math.floor(Math.random() * awsFiles.length + 1) ].Key
-    } 
+  // return items that are less than 30 days ago. these should be filtered from aws files list
+  const rawLog = logs.filter( (el) => {
+    // hours ago 
+    const dt = DateTime.fromISO(el.date).toUTC();
+    return Interval.fromDateTimes(dt, DateTime.utc()).count('days')-1 < 31;
+  });
 
-    function checkExists(logFiles, awsFiles) {
-      const filterRes = logFiles.filter( (el) => {
-        return awsFiles.includes(el.content)
-      });
-    
-      console.log(res.length > 0);
-    }
+  // make arrays of the two file obj sources
+  const awsFiles = awsFileObjects.map( (obj) => {
+    return settings.config.aws.S3_URL + obj.Key
+  });
+  const logContent = rawLog.map( (obj) => {
+    return obj.content
+  })
 
-  }
+  // filter the files out of aws list where they are in the logs list
+  let filteredFiles = awsFiles.filter( (el) => {
+    return !logContent.includes(el)
+  });
+
+  // return a random file from the filtered set
+  return filteredFiles[ Math.floor(Math.random() * filteredFiles.length + 1) ]
+
 }
 
