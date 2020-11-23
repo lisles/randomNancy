96b3a2a1070369159d2177089c0c309806a47422
Lee Isles <leeisles@gmail.com>
2020-11-23 10:05:22 -0500

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+git push origin master
+
+scp -i ~/.ssh/ec2-2019.pem googleCredsLogging.json ubuntu@54.157.195.221:~/randomNancy/nbdy/randomNancy/.
+
+scp -i ~/.ssh/ec2-2019.pem googleCredsLogging.json ubuntu@54.157.195.221:~/randomNancy/peeks/randomNancy/.
+
+scp -i ~/.ssh/ec2-2019.pem settings.json ubuntu@54.157.195.221:~/randomNancy/nbdy/randomNancy/.
+
+scp -i ~/.ssh/ec2-2019.pem settings.json ubuntu@54.157.195.221:~/randomNancy/peeks/randomNancy/.
